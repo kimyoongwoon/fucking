4127f7ae85b6fb74a19bb6e567059e89707d2119
kimyoongwoon <129961497+kimyoongwoon@users.noreply.github.com>
2025-06-27 02:34:34 +0900

    Fix scaling module imports

--- visualizations/scaling/color_scaling.js
+++ visualizations/scaling/color_scaling.js
@@ -1,1 +1,3 @@
-export { applyColorScaling } from '../graph_UI/graph_option/scaling/color/color_gradient.js';
+// Re-export the color scaling function used by visualization modules
+// Corrected relative path to the shared color gradient implementation
+export { applyColorScaling } from '../../graph_UI/graph_option/scaling/color/color_gradient.js';
